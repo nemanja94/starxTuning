c09a844cf78dc69cd10eda7ab2921b62cd6a1e77
Nemanja Andric <aandric.nemanja@gmail.com>
2023-01-27 11:09:27 +0100

    added front module

---
+++ front/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import AddUserForm from './components/users/addUserForm/AddUserForm';
+
+function App() {
+  return (
+    <div className="App">
+      <AddUserForm />
+    </div>
+  );
+}
+
+export default App;
